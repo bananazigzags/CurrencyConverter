5ed6a10788b7e03de557e254831d76ef5e4dfec5
bananazigzags <bananazigzags@gmail.com>
2022-02-27 22:05:03 +0300

    added reverse currencies and input/conversion  functionality

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,23 +1,65 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Entypo } from '@expo/vector-icons';
 
 import Home from '../screens/Home';
 import Options from '../screens/Options';
+import CurrencyList from '../screens/CurrencyList';
+import colors from '../constants/colors';
 
 const MainStack = createStackNavigator();
 const MainStackScreen = () => (
-  <MainStack.Navigator
-    // screenOptions={{headerShown: false}}
-    // initialRouteName="Options"
-  >
-    <MainStack.Screen name="Home" component={Home} options={{headerShown: false}}/>
-    <MainStack.Screen name="Options" component={Options} />
+  <MainStack.Navigator>
+    <MainStack.Screen name="Home" component={Home} options={{headerShown: false}} />
+    <MainStack.Screen 
+      name="Options"
+      component={Options}
+    />
+    <MainStack.Screen
+      name="CurrencyList" 
+      component={CurrencyList} 
+      options={({ navigation, route }) => ({
+      title: route.params && route.params.title,
+      headerLeft: null,
+      headerRight: () => (
+        <TouchableOpacity onPress={() => navigation.pop()} style={{paddingHorizontal: 10}}>
+          <Entypo name="cross" size={30} color={colors.blue} />
+        </TouchableOpacity>
+        )
+      })}
+    />
   </MainStack.Navigator>
 );
 
+const ModalStack = createStackNavigator();
+const ModalStackScreen = () => (
+  <ModalStack.Navigator>
+    <ModalStack.Screen
+      name="Main"
+      component={MainStackScreen}
+      options={{headerShown: false}}
+    />
+    <ModalStack.Screen
+      name="CurrencyList"
+      component={CurrencyList} 
+      options={({ navigation, route }) => ({
+      title: route.params && route.params.title,
+      headerBackVisible: false,
+      headerRight: () => (
+        <TouchableOpacity onPress={() => navigation.pop()}>
+          <Entypo name="cross" size={30} color={colors.blue} />
+        </TouchableOpacity>
+      )
+      })}
+    />
+  </ModalStack.Navigator>
+)
+
+
 export default () => (
   <NavigationContainer>
-    <MainStackScreen />
+    <ModalStackScreen />
   </NavigationContainer>
 );
---
+++ README.md
@@ -0,0 +1,2 @@
+A React Native App built with React Native School tutorial to learn React Native Basics.
+Test the app here: https://expo.dev/@bananazigzags/CurrencyConverter
